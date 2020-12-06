e7fb06f1d7d665ebc8d4cb92e9bd3d11f14414ad
SosnoviyBor <wormer007@gmail.com>
2020-12-06 17:37:08 +0200

    Доделал 8 лабу по ОП

--- Прога/Лаб.8/Main.java
+++ Прога/Лаб.8/Main.java
@@ -1,12 +1,28 @@
+import java.lang.Math;
+
 class Main {
 	public static void main (String args[]) {
-
+		Rectangle r1 = new Rectangle(3,4);
+		System.out.println(r1.expandToSquare().toString());
 	}
 }
 
-class Shape {
-	boolean isRound;
-	double square() {
+abstract class Shape {
+	private String name;
+
+	Shape (String name) {
+		this.name = name;
+	}
+
+	String getName () {
+		return name;
+	}
+
+	double getSquare () {
+		return 0;
+	}
+
+	double getPerimeter () {
 		return 0;
 	}
 }
@@ -16,21 +32,40 @@ class Rectangle extends Shape {
 	private double width;
 
 	Rectangle (double length, double width) {
+		super("Rectangle");
 		this.length = length;
 		this.width = width;
 	}
 	Rectangle () {
+		super("Rectangle");
 		this.length = 0;
 		this.width = 0;
 	}
-	Rectangle (Rectangle a) {
-		this.length = a.length;
-		this.width = a.width;
-	}
 
 	@Override
-	double square () {
+	public String toString() {
+		return "Shape{"+
+		"name="+super.getName()+
+		", length="+length+
+		", width="+width+
+		", square="+getSquare()+
+		", perimeter="+getPerimeter()+"}";
+	}
+
+	double getSquare () {
 		return length*width;
+	}
+
+	double getPerimeter () {
+		return 2*(length+width);
+	}
+
+	double getLength () {
+		return length;
+	}
+
+	double getWidth () {
+		return width;
 	}
 
 	Rectangle cutToSquare () {
@@ -40,11 +75,51 @@ class Rectangle extends Shape {
 		Rectangle res = new Rectangle(side, side);
 		return res;
 	}
+
 	Rectangle expandToSquare () {
 		double side;
 		if (width < length) {side = length;}
 		else {side = width;}
 		Rectangle res = new Rectangle(side, side);
+		return res;
+	}
+}
+
+class Circle extends Shape {
+	private double radius;
+
+	Circle (double radius) {
+		super("Circle");
+		this.radius = radius;
+	}
+	Circle () {
+		super("Circle");
+		this.radius = 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Shape{"+
+		"name="+super.getName()+
+		", radius="+radius+
+		", square="+getSquare()+
+		", perimeter="+getPerimeter()+"}";
+	}
+	
+	double getSquare () {
+		return Math.PI*radius*radius;
+	}
+
+	double getPerimeter () {
+		return 2*Math.PI*radius;
+	}
+
+	double getRadius () {
+		return radius;
+	}
+
+	Circle createOneMore (double newRadius) {
+		Circle res = new Circle (newRadius);
 		return res;
 	}
 }
