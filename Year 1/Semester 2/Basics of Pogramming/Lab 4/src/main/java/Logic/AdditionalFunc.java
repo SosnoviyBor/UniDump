34a36e92fde7261a0b0de338d18ec68a8e340393
SosnoviyBor <wormer007@gmail.com>
2021-04-28 23:26:15 +0300

    Чистим-чистим-чистим

--- Year 1/Semester 2/Basics of Pogramming/Lab 4/src/main/java/Logic/AdditionalFunc.java
+++ Year 1/Semester 2/Basics of Pogramming/Lab 4/src/main/java/Logic/AdditionalFunc.java
@@ -4,19 +4,9 @@ import java.util.ArrayList;
 
 public class AdditionalFunc {
     public static ArrayList<Institute> institutes = new ArrayList<>();
-//    public static void init() {
-//        Institute kpi = new Institute("KPI");
-//        Faculty FICT = new Faculty("FICT",kpi);
-//        Faculty IPSA = new Faculty("IPSA",kpi);
-//        Student korol = new Student("Korol Kirill","AC555555",87.3,FICT);
-//        Student bukreev = new Student("Bukreev Nikita", "AC666666",98.3,FICT);
-//        Student borya = new Student("Sosnoviy Bor","AC77777777",95.6,FICT);
-//        Student goncharov = new Student("Goncharov Anton","AE333333",75.3,IPSA);
-//        Student ivanov = new Student("Ivanov Ivan","AE666666",95,IPSA);
-//        institutes.add(kpi);
-//    }
+
     static {
-            Institute kpi = new Institute("KPI");
+        Institute kpi = new Institute("KPI");
         Faculty FICT = new Faculty("FICT",kpi);
         Faculty IPSA = new Faculty("IPSA",kpi);
         Student korol = new Student("Korol Kirill","AC555555",87.3,FICT);
