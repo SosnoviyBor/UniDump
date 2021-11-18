c2a773b5ae16a7dc4931fc5d0e651ce09e309f5d
SosnoviyBor <wormer007@gmail.com>
2021-11-18 19:34:33 +0200

    Скоро доделаю

---
+++ Year 2/Web/Lab 3/viewScript.js
@@ -0,0 +1,5 @@
+const blockX = document.getElementById('x_header');
+const blockY = document.getElementById('y_header');
+
+blockX.onclick = () => window.location.replace("index.html")
+blockY.onclick = () => window.location.replace("index.html")
