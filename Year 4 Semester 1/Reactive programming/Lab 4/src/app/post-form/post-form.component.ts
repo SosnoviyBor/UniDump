e50737ced178ca502d2a271565959ef847bdcf46
SosnoviyBor <wormer007@gmail.com>
2023-11-04 17:07:46 +0200

    and another one

--- Year 4 Semester 1/Reactive programming/Lab 4/src/app/post-form/post-form.component.ts
+++ Year 4 Semester 1/Reactive programming/Lab 4/src/app/post-form/post-form.component.ts
@@ -1,16 +1,21 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Post } from '../app.component';
+
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css'],
 })
+
 export class PostFormComponent implements OnInit {
   @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
   title = '';
   text = '';
+
   constructor() {}
+
   ngOnInit(): void {}
+
   addPost() {
     if (this.title.trim() && this.text.trim()) {
       const post: Post = {
