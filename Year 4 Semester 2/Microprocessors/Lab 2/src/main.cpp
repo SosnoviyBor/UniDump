e765de10963e98a7ba23bc9d3add477c6dfa9f7a
SosnoviyBor <wormer007@gmail.com>
2024-02-23 01:39:04 +0200

    neat

--- Year 4 Semester 2/Microprocessors/Lab 2/src/main.cpp
+++ Year 4 Semester 2/Microprocessors/Lab 2/src/main.cpp
@@ -1,6 +1,52 @@
 #include "stm32f103x6.h"
 #include <string>
 
+void init_ADC() {
+    // enable ADC and make it continuous
+    // p0 by default
+    ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_CONT;
+    ADC1->SMPR2 |= ADC_SMPR2_SMP0;
+    ADC1->CR2 |= ADC_CR2_SWSTART; // launch ADC
+}
+
+void init_USART1n2() {
+    // output USART
+    // bound to p9 port cuz lord Jesus said so
+    USART1->BRR = 8000000 / 9600; // baud rate
+    USART1->CR1 |= USART_CR1_TE | USART_CR1_UE; // transmit and USART enable
+
+    // input USART
+    // bound to p3
+    NVIC_EnableIRQ(USART2_IRQn);
+    USART2->BRR = 8000000 / 9600;
+    USART2->CR1 |= USART_CR1_RE | USART_CR1_UE; // recieve and USART enable
+}
+
+void init_timer(bool enabled) {
+    NVIC_EnableIRQ(TIM2_IRQn);
+    TIM2->PSC = 8000-1; // prescale clock to 1khz
+    TIM2->ARR = 1000-1; // proc 1 sec
+    TIM2->DIER = TIM_DIER_UIE; // enable interruptions
+    if (enabled) {
+        TIM2->CR1 = TIM_CR1_CEN;
+    }
+}
+
+void init_PWM(bool enabled) {
+    GPIOA->CRH &= ~(GPIO_CRH_MODE8 | GPIO_CRH_CNF8);
+    GPIOA->CRH |= GPIO_CRH_MODE8_0 | GPIO_CRH_CNF8_1;
+    AFIO->MAPR |= AFIO_MAPR_TIM1_REMAP_0; // make alt function proc on p7
+    TIM1->PSC = 8000-1; // 1khz
+    TIM1->ARR = 1000-1; // proc 1 sec
+    TIM1->CCR1 = 250-1; // pulse length to 1/4 sec
+    TIM1->CCER |= TIM_CCER_CC1E | TIM_CCER_CC1P; // enable capture/compare, invert polarity
+    TIM1->BDTR |= TIM_BDTR_MOE; // enable output
+    TIM1->CR1 |= TIM_CR1_CEN; // enable counter
+    if (enabled) {
+        TIM1->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+    }
+}
+
 void send(char s) {
         while ((USART1->SR & USART_SR_TC) == 0); // wait for completion
         USART1->DR = s;
@@ -26,41 +72,11 @@ int main() {
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN | RCC_APB1ENR_TIM2EN;
     RCC->APB2ENR |= RCC_APB2ENR_USART1EN | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_ADC1EN | RCC_APB2ENR_TIM1EN;
 
-    // enable ADC and make it continuous
-    // p0 by default
-    ADC1->CR2 |= ADC_CR2_ADON | ADC_CR2_CONT;
-    ADC1->SMPR2 |= ADC_SMPR2_SMP0;
-    ADC1->CR2 |= ADC_CR2_SWSTART; // launch ADC
-
-    // output USART
-    // bound to p9 port cuz lord Jesus said so
-    USART1->BRR = 8000000 / 9600; // baud rate
-    USART1->CR1 |= USART_CR1_TE | USART_CR1_UE; // transmit and USART enable
-
-    // input USART
-    // bound to p3
-    NVIC_EnableIRQ(USART2_IRQn);
-    USART2->BRR = 8000000 / 9600;
-    USART2->CR1 |= USART_CR1_RE | USART_CR1_UE; // recieve and USART enable
-
-    // prev lab code
-    // LED
-    GPIOA->CRL |= GPIO_CRL_MODE1_0;
-    // Timer
-    NVIC_EnableIRQ(TIM2_IRQn);
-    TIM2->PSC = 8000-1; // prescale clock to 1khz
-    TIM2->ARR = 1000-1; // proc 1 sec
-    TIM2->DIER = TIM_DIER_UIE; // enable interruptions
-    // PWM
-    GPIOA->CRH &= ~(GPIO_CRH_MODE8 | GPIO_CRH_CNF8);
-    GPIOA->CRH |= GPIO_CRH_MODE8_0 | GPIO_CRH_CNF8_1;
-    AFIO->MAPR |= AFIO_MAPR_TIM1_REMAP_0; // make alt function proc on p7
-    TIM1->PSC = 8000-1; // 1khz
-    TIM1->ARR = 1000-1; // proc 1 sec
-    TIM1->CCR1 = 250-1; // pulse length to 1/4 sec
-    TIM1->CCER |= TIM_CCER_CC1E | TIM_CCER_CC1P; // enable capture/compare, invert polarity
-    TIM1->BDTR |= TIM_BDTR_MOE; // enable output
-    TIM1->CR1 |= TIM_CR1_CEN; // enable counter
+    init_ADC();
+    init_USART1n2();
+    GPIOA->CRL |= GPIO_CRL_MODE1_0; // init LED lmao
+    init_timer(false);
+    init_PWM(false);
 
     send("I'm a reciever ma'am, not the other one.\r\n"
          "==========================\r\n"
@@ -75,6 +91,7 @@ int main() {
          "1-9 - Set timer's delay to X seconds\r\n"
          "==========================\r\n"
     );
+    
     char tmp[200];
     while (true) {
         switch (tolower(USART2->DR)) {
