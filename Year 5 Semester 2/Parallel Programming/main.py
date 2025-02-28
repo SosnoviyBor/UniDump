9326291dcfb48fd4fe81dab6161fa65e391c62f2
SosnoviyBor <wormer007@gmail.com>
2025-02-28 13:49:03 +0200

    fixed critical bug and reworked info output

--- Year 5 Semester 2/Parallel Programming/main.py
+++ Year 5 Semester 2/Parallel Programming/main.py
@@ -1,9 +1,20 @@
 from model import Model
 
 if __name__ == "__main__":
-    m = Model(crystal_len=20,
-              atom_count=200,
-              move_left_chance=.5)
     
-    states = m.run_multiprocess(snapshot_count=10, iters_per_snapshot=50000)
-    states = m.run_singleprocess(snapshot_count=10, iters_per_snapshot=50000)
+    (Model(crystal_len=20,
+           atom_count=200,
+           move_left_chance=.5)
+     .run(multiprocessed=True,
+          print_snapshots=False,
+          snapshot_count=10,
+          iters_per_snapshot=50_000))
+    
+    
+    (Model(crystal_len=20,
+           atom_count=200,
+           move_left_chance=.5)
+     .run(multiprocessed=False,
+          print_snapshots=False,
+          snapshot_count=10,
+          iters_per_snapshot=50_000))
--- Year 5 Semester 2/Parallel Programming/model.py
+++ Year 5 Semester 2/Parallel Programming/model.py
@@ -1,20 +1,58 @@
 import multiprocessing as mp
 import random
+import time
 
-import utils.Coloring as clr
-import utils.ProgressBar as bar
+import utils.coloring as clr
+import utils.progress_bar as bar
+import utils.txt as txt
 
 class Model:
     def __init__(self, crystal_len:int, atom_count:int, move_left_chance:float):
         self.crystal_len = crystal_len
         # atoms are represented by their position in crystal
         self.atoms = [0 for _ in range(atom_count)]
-        self.randoms = [random.Random(i) for i in range(atom_count)]
         self.move_left_chance = move_left_chance
         self.crystal_states = []
     
     
-    def run_multiprocess(self, snapshot_count:int, iters_per_snapshot:int) -> list[list[int]]:
+    def run(self,
+            multiprocessed:bool,
+            print_snapshots:bool,
+            snapshot_count:int,
+            iters_per_snapshot:int) -> None:
+        """Simply a wrapper function for run_multiprocesse() and run_singleprocess()"""
+        if multiprocessed:
+            print(txt.multi)
+        else:
+            print(txt.single)
+            
+        print(f"> Довжина кристалу:           {self.crystal_len}\n"+
+              f"> Кількість атомів:           {len(self.atoms)}\n"+
+              f"> Ймовірність переходу вліво: {self.move_left_chance * 100}%\n"+
+              f"> Кількість знімків:          {snapshot_count}\n"+
+              f"> Ітерацій між знімками:      {iters_per_snapshot}\n")
+
+        if multiprocessed:
+            start = time.perf_counter()
+            self.run_multiprocess(snapshot_count, iters_per_snapshot)
+            end = time.perf_counter()
+        else:
+            start = time.perf_counter()
+            self.run_singleprocess(snapshot_count, iters_per_snapshot)
+            end = time.perf_counter()
+        
+        if print_snapshots:
+            print("> Знімки:")
+            for snapshot in self.crystal_states:
+                print(snapshot)
+            print()
+        
+        print(f"> Час виконання: "+
+              clr.wrap(str(round(end - start, 1)) + ' сек.', clr.Color.FG.GREEN) +
+              "\n")
+    
+    
+    def run_multiprocess(self, snapshot_count:int, iters_per_snapshot:int) -> None:
         # init progressbar
         bar.display(0, snapshot_count)
         manager = mp.Manager()
@@ -23,11 +61,10 @@ class Model:
             moved_atoms = manager.list([])
             # prepare args for mapping
             args = [(self.atoms[i],
-                        iters_per_snapshot,
-                        self.move_left_chance,
-                        self.crystal_len,
-                        self.randoms[i],
-                        moved_atoms)
+                     iters_per_snapshot,
+                     self.move_left_chance,
+                     self.crystal_len,
+                     moved_atoms)
                     for i in range(len(self.atoms))]
             
             # literally all the multithreading
@@ -43,12 +80,12 @@ class Model:
             
         # print the crystal the pretty way
         self.print_crystal()
-        return self.crystal_states
     
     
     def atom_mover(self, args):
         # unpack args
-        atom, iters, move_left_chance, crystal_len, local_random, results = args
+        atom, iters, move_left_chance, crystal_len, results = args
+        local_random = random.Random()
         
         for _ in range(iters):
             move_chance = local_random.uniform(0, 1)
@@ -66,6 +103,7 @@ class Model:
         # init progressbar
         bar_total = snapshot_count * len(self.atoms)
         iters_total = 0
+        local_random = random.Random(0)
         bar.display(0, bar_total)
         
         # for each snapshot
@@ -74,7 +112,7 @@ class Model:
             for i in range(len(self.atoms)):
                 # do X iters
                 for _ in range(iters_per_snapshot):
-                    move_chance = random.uniform(0, 1)
+                    move_chance = local_random.uniform(0, 1)
                     # move left
                     if move_chance > self.move_left_chance and not self.atoms[i] >= self.crystal_len-1:
                         self.atoms[i] += 1
@@ -89,6 +127,8 @@ class Model:
             # snapshot the crystal state
             self.crystal_states.append(self.get_crystal_struct())
             
+        # just to add empty line after progressbar
+        print()
         # print the crystal the pretty way
         self.print_crystal()
         return self.crystal_states
@@ -105,13 +145,18 @@ class Model:
             msg[1] += str(val) + "┊"
             msg[2] += "═" * digits + "╧"
         msg[0] = msg[0][:-1] + "╗\n"
-        msg[1] = msg[1][:-1] + "║" + f" Total: {sum(self.get_crystal_struct())}\n"
+        msg[1] = msg[1][:-1] + "║\n"
         msg[2] = msg[2][:-1] + "╝"
-        print(clr.wrap("".join(msg), clr.Color.FG.CYAN))
+        
+        print("> Кінцевий вигляд кристалу\n" +
+              clr.wrap("".join(msg), clr.Color.FG.CYAN) + "\n"
+            #   left it in case you would want to check if all atoms are in place
+            #   + f"> Усього атомів: {sum(self.get_crystal_struct())}\n"
+              )
     
     
     def get_crystal_struct(self) -> list[int]:
-        # cunt atoms in each segment of crystal
+        # count atoms in each segment of crystal
         crystal = [0 for _ in range(self.crystal_len)]
         
         for atom in self.atoms:
